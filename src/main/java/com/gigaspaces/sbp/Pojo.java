d8eb6b094e1accc3082213bd2f4c016e942a7b8d
Jason Nerothin <jason.nerothin@kohls.com>
2014-06-15 22:14:24 -0400

    adding a pojo and eliminating build warnings

---
+++ src/main/java/com/gigaspaces/sbp/Pojo.java
@@ -0,0 +1,87 @@
+package com.gigaspaces.sbp;
+
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 6/15/14
+ * Time: 10:09 PM
+ */
+@SpaceClass
+public class Pojo {
+
+    private String spaceId;
+    private Integer routingId;
+    private String payload;
+
+    @SpaceId
+    public String getSpaceId() {
+        return spaceId;
+    }
+
+    public void setSpaceId(String spaceId) {
+        this.spaceId = spaceId;
+    }
+
+    @SpaceRouting
+    public Integer getRoutingId() {
+        return routingId;
+    }
+
+    public void setRoutingId(Integer routingId) {
+        this.routingId = routingId;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pojo pojo = (Pojo) o;
+
+        if (payload != null ? !payload.equals(pojo.payload) : pojo.payload != null) return false;
+        if (routingId != null ? !routingId.equals(pojo.routingId) : pojo.routingId != null) return false;
+        if (spaceId != null ? !spaceId.equals(pojo.spaceId) : pojo.spaceId != null) return false;
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        int result = spaceId != null ? spaceId.hashCode() : 0;
+        result = 31 * result + (routingId != null ? routingId.hashCode() : 0);
+        result = 31 * result + (payload != null ? payload.hashCode() : 0);
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Pojo{");
+        sb.append("spaceId='").append(spaceId).append('\'');
+        sb.append(", routingId=").append(routingId);
+        sb.append(", payload='").append(payload).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
